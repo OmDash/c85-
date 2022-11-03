23a5357508febfcc607042495dfd7197acb3d97b
OmDash <78514786+OmDash@users.noreply.github.com>
2022-11-02 22:02:22 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDQ21yqZ1Z0do_QPtXe2zP_ilGa-9WWtOA",
+  authDomain: "storytellingapp-b2ba5.firebaseapp.com",
+  databaseURL: "https://storytellingapp-b2ba5-default-rtdb.firebaseio.com",
+  projectId: "storytellingapp-b2ba5",
+  storageBucket: "storytellingapp-b2ba5.appspot.com",
+  messagingSenderId: "290696570262",
+  appId: "1:290696570262:web:e8fc584a8b80613570a9b7"
+};
